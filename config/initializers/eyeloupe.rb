1b82187c18bfa141c364f3b274871d8898cb42bc
Ben Purinton <ben.purinton@gmail.com>
2023-08-01 14:39:42 +0000

    initializer and eyeloupe db setup

---
+++ config/initializers/eyeloupe.rb
@@ -0,0 +1,7 @@
+Eyeloupe.configure do |config|
+  config.excluded_paths = %w[assets favicon.ico service-worker.js manifest.json]
+  config.capture = Rails.env.development?
+  config.openai_access_key = ENV.fetch("OPENAI_KEY")
+  config.openai_model = "gpt-3.5"
+  config.database = "eyeloupe"
+end
