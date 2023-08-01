a4bc97b8653e374f0c735b121e1ec51257025d96
Ben Purinton <ben.purinton@gmail.com>
2023-08-01 15:06:25 +0000

    update gpt model

--- config/initializers/eyeloupe.rb
+++ config/initializers/eyeloupe.rb
@@ -2,6 +2,6 @@ Eyeloupe.configure do |config|
   config.excluded_paths = %w[assets favicon.ico service-worker.js manifest.json]
   config.capture = Rails.env.development?
   config.openai_access_key = ENV.fetch("OPENAI_KEY")
-  config.openai_model = "ada"
+  config.openai_model = "gpt-3.5-turbo"
   config.database = "eyeloupe"
 end
