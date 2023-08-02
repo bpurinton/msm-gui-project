a705ae124076205d1e6fbf43075e7e910dcc55df
Ben Purinton <ben.purinton@gmail.com>
2023-08-02 03:26:56 +0000

    rails signalman:install:migrations

---
+++ db/migrate/20230802032646_create_signalman_events.signalman.rb
@@ -0,0 +1,16 @@
+# This migration comes from signalman (originally 20230729122215)
+class CreateSignalmanEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :signalman_events do |t|
+      t.string :name
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.float :duration
+      t.json :payload
+
+      t.timestamps
+    end
+
+    add_index :signalman_events, [:name, :duration]
+  end
+end
